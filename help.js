62784e2ed9571c1bbae6c1d29752e241d9f3c979
Wsn <wsn@bk.ru>
2020-12-22 01:00:13 +0300

    First commit

---
+++ help.js
@@ -0,0 +1,18 @@
+// Переменные
+let $perem = 1;
+let _perem = 2;
+let perem = 3;
+const leftBorderWidth = 1;
+// не использовать
+var name = 'Ivan';
+
+// Типы данных
+
+//null 
+//undefined
+// Sumbol
+//Bigint 
+
+// 4 / 0 = Infinity
+// 'string' * 9 = NaN
+//https://githowto.com/ru
